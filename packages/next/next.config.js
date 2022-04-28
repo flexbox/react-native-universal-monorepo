5af016213f61b322c4842b32f6c563161d3fa772
David Leuliette <dleuliette@gmail.com>
2022-04-28 09:20:17 +0200

    bump nextjs

--- packages/next/next.config.js
+++ packages/next/next.config.js
@@ -47,4 +47,24 @@ const nextConfig = {
 const withImages = require("next-images");
 const withFonts = require("next-fonts");
 
-module.exports = withImages(withFonts(nextConfig));
+const { withExpo } = require("@expo/next-adapter");
+const withPlugins = require("next-compose-plugins");
+const withTM = require("next-transpile-modules")([
+  "react-native-web",
+  "react-native-svg",
+  "native-base",
+  "react-native-svg",
+]);
+
+// module.exports = withImages(withFonts(nextConfig));
+
+module.exports = withPlugins(
+  [
+    withTM,
+    [withFonts, { projectRoot: __dirname }],
+    [withImages, { projectRoot: __dirname }],
+    [withExpo, { projectRoot: __dirname }],
+    // your plugins go here.
+  ],
+  nextConfig
+);
